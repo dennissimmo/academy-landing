f9fe72775ac019597c1552ddd459803905db7d5d
dennissimmo <denchik16062001@gmail.com>
2022-04-14 23:56:41 +0300

    [Header] Keep header at the top on scroll, make header translusent, add main button hover,

--- src/js/app.js
+++ src/js/app.js
@@ -4,3 +4,10 @@ import { menuInit } from "./files/functions.js";
 
 // Burger menu
 menuInit();
+
+// Work with scroll
+import * as flScroll from "./files/scroll/scroll.js";
+flScroll.headerScroll();
+
+// Popup 
+import './libs/popup.js'
